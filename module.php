6e584970acad3dc2a53e445ca8e7a42eb4152aa9
nadine-shi <nadine@afterlogic.com>
2016-11-17 11:37:05 +0300

    Contacts - storages refactoring

--- module.php
+++ module.php
@@ -5,10 +5,25 @@ class PersonalContactsModule extends AApiModule
 	public function init() 
 	{
 		$this->subscribeEvent('Contacts::GetStorage', array($this, 'onGetStorage'));
+		$this->subscribeEvent('Contacts::GetContacts::before', array($this, 'onBeforeGetContacts'));
 	}
 	
 	public function onGetStorage(&$aStorages)
 	{
 		$aStorages[] = 'personal';
+	}
+	
+	public function onBeforeGetContacts(&$aArgs, &$mResult)
+	{
+		if (isset($aArgs['Storage']) && $aArgs['Storage'] === 'personal')
+		{
+			$iUserId = \CApi::getAuthenticatedUserId();
+			if (!isset($aArgs['Filters']) || !is_array($aArgs['Filters']))
+			{
+				$aArgs['Filters'] = array();
+			}
+			$aArgs['Filters']['Storage'] = 'personal';
+			$aArgs['Filters']['IdUser'] = $iUserId;
+		}
 	}
 }
