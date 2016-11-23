d135500eca9b2295b7fac2e4080aa8aa56287b87
nadine-shi <nadine@afterlogic.com>
2016-11-23 19:30:15 +0300

    Contacts - storages refactoring

--- module.php
+++ module.php
@@ -5,12 +5,41 @@ class PersonalContactsModule extends AApiModule
 	public function init() 
 	{
 		$this->subscribeEvent('Contacts::GetStorage', array($this, 'onGetStorage'));
+		$this->subscribeEvent('AdminPanelWebclient::DeleteEntity::before', array($this, 'onBeforeDeleteEntity'));
 		$this->subscribeEvent('Contacts::GetContacts::before', array($this, 'onBeforeGetContacts'));
 	}
 	
 	public function onGetStorage(&$aStorages)
 	{
 		$aStorages[] = 'personal';
+	}
+	
+	public function onBeforeDeleteEntity(&$aArgs, &$mResult)
+	{
+		if ($aArgs['Type'] === 'User')
+		{
+			$oContactsDecorator = \CApi::GetModuleDecorator('Contacts');
+			if ($oContactsDecorator)
+			{
+				$aFilters = [
+					'$AND' => [
+						'IdUser' => [$aArgs['Id'], '='],
+						'Storage' => ['personal', '=']
+					]
+				];
+				$oApiContactsManager = $oContactsDecorator->GetApiContactsManager();
+				$aUserContacts = $oApiContactsManager->getContactItems(EContactSortField::Name, ESortOrder::ASC, 0, 0, $aFilters, 0);
+				if (count($aUserContacts) > 0)
+				{
+					$aContactIds = [];
+					foreach ($aUserContacts as $oContact)
+					{
+						$aContactIds[] = $oContact->iId;
+					}
+					$oContactsDecorator->DeleteContacts($aContactIds);
+				}
+			}
+		}
 	}
 	
 	public function onBeforeGetContacts(&$aArgs, &$mResult)
@@ -22,7 +51,7 @@ class PersonalContactsModule extends AApiModule
 			{
 				$aArgs['Filters'] = array();
 			}
-			$aArgs['Filters']['$AND'] = [
+			$aArgs['Filters'][]['$AND'] = [
 				'IdUser' => [$iUserId, '='],
 				'Storage' => ['personal', '='],
 			];
