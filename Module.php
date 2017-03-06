4120807e2b1b08308234b152c5ab8ebd74dd7c3f
vasil <vasil.sokolov@gmail.com>
2017-03-06 11:58:10 +0300

    Second stem of renaming of modules.php

---
+++ Module.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Aurora\Modules\PersonalContacts;
+
+class Module extends \Aurora\System\Module\AbstractModule
+{
+	public function init() 
+	{
+		$this->subscribeEvent('Contacts::GetStorage', array($this, 'onGetStorage'));
+		$this->subscribeEvent('AdminPanelWebclient::DeleteEntity::before', array($this, 'onBeforeDeleteEntity'));
+		$this->subscribeEvent('Contacts::CreateContact::before', array($this, 'onBeforeCreateContact'));
+		$this->subscribeEvent('Contacts::GetContacts::before', array($this, 'prepareFiltersFromStorage'));
+		$this->subscribeEvent('Contacts::Export::before', array($this, 'prepareFiltersFromStorage'));
+	}
+	
+	public function onGetStorage(&$aStorages)
+	{
+		$aStorages[] = 'personal';
+	}
+	
+	public function onBeforeDeleteEntity(&$aArgs, &$mResult)
+	{
+		if ($aArgs['Type'] === 'User')
+		{
+			$oContactsDecorator = \Aurora\System\Api::GetModuleDecorator('Contacts');
+			if ($oContactsDecorator)
+			{
+				$aFilters = [
+					'$AND' => [
+						'IdUser' => [$aArgs['Id'], '='],
+						'Storage' => ['personal', '=']
+					]
+				];
+				$oApiContactsManager = $oContactsDecorator->GetApiContactsManager();
+				$aUserContacts = $oApiContactsManager->getContacts(\EContactSortField::Name, \ESortOrder::ASC, 0, 0, $aFilters, '');
+				if (count($aUserContacts) > 0)
+				{
+					$aContactIds = [];
+					foreach ($aUserContacts as $oContact)
+					{
+						$aContactIds[] = $oContact->EntityId;
+					}
+					$oContactsDecorator->DeleteContacts($aContactIds);
+				}
+			}
+		}
+	}
+	
+	public function onBeforeCreateContact(&$aArgs, &$mResult)
+	{
+		if (isset($aArgs['Contact']))
+		{
+			if (!isset($aArgs['Contact']['Storage']) || $aArgs['Contact']['Storage'] === '')
+			{
+				$aArgs['Contact']['Storage'] = 'personal';
+			}
+		}
+	}
+	
+	public function prepareFiltersFromStorage(&$aArgs, &$mResult)
+	{
+		if (isset($aArgs['Storage']) && ($aArgs['Storage'] === 'personal' || $aArgs['Storage'] === 'all'))
+		{
+			$iUserId = \Aurora\System\Api::getAuthenticatedUserId();
+			if (!isset($aArgs['Filters']) || !\is_array($aArgs['Filters']))
+			{
+				$aArgs['Filters'] = array();
+			}
+			$aArgs['Filters'][]['$AND'] = [
+				'IdUser' => [$iUserId, '='],
+				'Storage' => ['personal', '='],
+			];
+		}
+	}
+}
