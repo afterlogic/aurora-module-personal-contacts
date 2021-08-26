150d33ca385ced2ff3f96c150877a6b82ee0944e
sash04ek <a.ovcharov@afterlogic.com>
2021-08-26 12:23:53 +0300

    Implement ability to create several custom address books.

--- Module.php
+++ Module.php
@@ -81,6 +81,12 @@ class Module extends \Aurora\System\Module\AbstractModule
 
 	 public function prepareFiltersFromStorage(&$aArgs, &$mResult)
 	{
+		$iAddressBookId = 0;
+		if (substr($aArgs['Storage'], 0, 11) === 'addressbook') {
+			$iAddressBookId = (int) substr($aArgs['Storage'], 11);
+			$sStorage = 'addressbook';
+		}
+
 		if (isset($aArgs['Storage']) && ($aArgs['Storage'] === self::$sStorage || $aArgs['Storage'] === StorageType::All || $aArgs['Storage'] === StorageType::Collected))
 		{
 			$iUserId = isset($aArgs['UserId']) ? $aArgs['UserId'] : \Aurora\System\Api::getAuthenticatedUserId();
@@ -98,37 +104,37 @@ class Module extends \Aurora\System\Module\AbstractModule
 				$bAuto = true;
 			}
 			
+			$aFilter = [
+				'IdUser' => [$iUserId, '='],
+				'Storage' => [$sStorage, '=']
+			];
+
+			if ($sStorage === 'addressbook')
+			{
+				$aFilter['AddressBookId'] = [$iAddressBookId, '='];
+			}
+
 			if (isset($aArgs['SortField']) && $aArgs['SortField'] === \Aurora\Modules\Contacts\Enums\SortField::Frequency)
 			{
-				$mResult[]['$AND'] = [
-					'IdUser' => [$iUserId, '='],
-					'Storage' => [self::$sStorage, '='],
-					'Frequency' => [-1, '!='],
-					'DateModified' => ['NULL', 'IS NOT']
-				];
+				$aFilter['Frequency'] = [-1, '!='];
+				$aFilter['DateModified'] = ['NULL', 'IS NOT'];
 			}
 			else
 			{
 				if (!$bAuto)
 				{
-					$mResult[]['$AND'] = [
-						'IdUser' => [$iUserId, '='],
-						'Storage' => [self::$sStorage, '='],
-						'$OR' => [
-							'1@Auto' => [false, '='],
-							'2@Auto' => ['NULL', 'IS']
-						]
+					$aFilter['$OR'] = [
+						'1@Auto' => [false, '='],
+						'2@Auto' => ['NULL', 'IS']
 					];
 				}
 				else
 				{
-					$mResult[]['$AND'] = [
-						'IdUser' => [$iUserId, '='],
-						'Storage' => [self::$sStorage, '='],
-						'Auto' => [true, '=']
-					];
+					$aFilter['Auto'] = [true, '='];
+
 				}
 			}
+			$mResult[]['$AND'] = $aFilter;
 		}
 	}	
 	
